f818613f953f9b37960cd2a64a4a68369e7832ed
antoine310 <antoine.vallat31@gmail.com>
2026-04-07 10:58:33 +0200

    ajout log vivado

--- core/timewarp.sv
+++ core/timewarp.sv
@@ -36,8 +36,8 @@ module timewarp
     // Lock le commit 
     output logic protect_en_o
 );
-    logic [$clog2(HIT_TIME+1)-1:0] compteur_hit;
-    logic [$clog2(STALL_COMMIT+1)-1:0] compteur_stall;
+    logic [5:0] compteur_hit;
+    logic [5:0] compteur_stall;
 
     logic hit_en ; // Signal d'un Load Hit en cours, Attente si une lecture va s'effectuer dessus 
     logic [2:0] dcache_hit_q; // Compteur pour prendre en compte les load Hit pas encore arriver au commit
@@ -49,7 +49,6 @@ module timewarp
         // Si on eu un load hit et une lecture, on commence un stall du pipeline.
         if (hit_en && csr_lecture_cycle_i) begin 
             protect_en_o = 1'b1; 
-            $display("TIMEWARP PROTECTION ENABLED");
         // On continue le temps du compteur.
         end else if (compteur_stall !=  0) begin
             protect_en_o = 1'b1;
@@ -69,7 +68,7 @@ module timewarp
             // Si on a un load commit et que c'était un hit, on commence le compteur et on active le hit_en.
             if ((dcache_hit_q>0) && load_commit_i) begin
                 hit_en <= 1'b1; 
-                compteur_hit <= HIT_TIME[$bits(compteur_hit)-1:0];
+                compteur_hit <= HIT_TIME;
             end else if (compteur_hit != 0) begin
                 compteur_hit <= compteur_hit - 1 ; 
                 if (compteur_hit == 1) begin
@@ -79,54 +78,12 @@ module timewarp
             // Si on a une Csr lecture commit et un hit_en, on consomme le Hit et on demarre le stall du pipeline pendant x cycle.
             if (hit_en && csr_lecture_cycle_i) begin 
                 hit_en <= 1'b0; 
-                compteur_stall <= STALL_COMMIT[$bits(compteur_stall)-1:0];
+                compteur_stall <= STALL_COMMIT;
             end else if (compteur_stall !=  0) begin
                 compteur_stall <= compteur_stall - 1 ; 
             end 
 
         end 
-    end
-
-    logic protect_en_q;
-    logic hit_en_q;
-    logic csr_cycle_q;
-    logic [2:0] dcache_hit_c;    
-    int nb_cycle;
-    logic load_commit_q;
-
-    always_ff @(posedge clk_i or negedge rst_ni) begin
-        if (!rst_ni) begin
-            protect_en_q  <= 1'b0;
-            hit_en_q      <= 1'b0;
-            dcache_hit_c  <= '0;
-            csr_cycle_q   <= 1'b0;
-            nb_cycle      <= 0;
-        end else begin
-
-            if (dcache_hit_c != dcache_hit_q)
-                $display("[cycle %0d] dcache_hit_cnt -> %0d", nb_cycle, dcache_hit_q);
-        
-            if (protect_en_o != protect_en_q)
-                $display("[cycle %0d] protect_en_o -> %0b \n", nb_cycle, protect_en_o);
-
-            if (hit_en != hit_en_q)
-                $display("[cycle %0d] hit_en -> %0b \n ", nb_cycle, hit_en);
-
-            if (csr_lecture_cycle_i != csr_cycle_q)
-                $display("[cycle %0d] csr_lecture_cycle -> %0b \n" , nb_cycle, csr_lecture_cycle_i);
-
-            if (load_commit_i != load_commit_q)
-                $display("[cycle %0d] load_commit_i -> %0b \n", nb_cycle, load_commit_i);
-
-    
-            protect_en_q <= protect_en_o;
-            hit_en_q <= hit_en;
-            csr_cycle_q <= csr_lecture_cycle_i;
-            dcache_hit_c <= dcache_hit_q;
-            load_commit_q <= load_commit_i;
-            nb_cycle <= nb_cycle + 1;
-
-        end
     end
 
 endmodule 
