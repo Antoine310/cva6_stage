aa076c600f8275422bbe637fa7342fd441180541
Antoine310 <antoine.vallat31@gmail.com>
2026-05-12 11:43:15 +0200

    modif val

--- core/timewarp.sv
+++ core/timewarp.sv
@@ -20,8 +20,8 @@ module timewarp
 #(
     parameter config_pkg::cva6_cfg_t CVA6Cfg = config_pkg::cva6_cfg_empty,
     parameter type dcache_req_o_t = logic,
-    parameter int HIT_TIME = 30,    // Delais Hit présent, HIT_TIME > 0 
-    parameter int CHARGE_TIME = 10 // temps ajouter au compteur de la charge 
+    parameter int HIT_TIME = 50,    // Delais Hit présent, HIT_TIME > 0 
+    parameter int CHARGE_TIME = 30 // temps ajouter au compteur de la charge 
 ) (
     // Subsystem Clock - SUBSYSTEM
     input logic clk_i,
